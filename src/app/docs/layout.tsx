a0e5007d225ce707ef97d64b1b607bc495664ead
Ingvar4 <jamessoer91@gmail.com>
2026-05-23 13:32:53 +0300

    Update docs tabs

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -5,7 +5,24 @@ import { baseOptions } from '@/lib/layout.shared';
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
     <DocsLayout tree={source.getPageTree()} {...baseOptions()}
-    
+    tabs={[
+    {
+      title: 'Learn',
+      description: 'Изучение react',
+      // active for `/docs/components` and sub routes like `/docs/components/button`
+      url: '/docs',
+      // optionally, you can specify a set of urls which activates the item
+      // urls: new Set(['/docs/test', '/docs/components']),
+    },
+    {
+      title: 'Components',
+      description: 'Hello World!',
+      // active for `/docs/components` and sub routes like `/docs/components/button`
+      url: '/docs/components',
+      // optionally, you can specify a set of urls which activates the item
+      // urls: new Set(['/docs/test', '/docs/components']),
+    },
+  ]}
     >
       {children}
     </DocsLayout>
