7e6b1788db1dc366da5f4606f93c6a5f61020a93
Ingvar4 <jamessoer91@gmail.com>
2026-06-01 12:42:04 +0300

    Update docs

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,17 +9,3 @@
 //     sitemap: 'https://howto-react.ru/sitemap.xml',
 //   };
 // }
-
-import type { MetadataRoute } from 'next';
-
-export const dynamic = 'force-static';
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
-    sitemap: 'https://Ingvar4.github.io/react_doc/sitemap.xml',
-  };
-}
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,21 +1,21 @@
-import type { MetadataRoute } from 'next';
-import { source } from '@/lib/source';
+// import type { MetadataRoute } from 'next';
+// import { source } from '@/lib/source';
 
-const baseUrl = 'https://howto-react.ru';
+// const baseUrl = 'https://howto-react.ru';
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const pages: MetadataRoute.Sitemap = source.getPages().map((page) => ({
-    url: `${baseUrl}${page.url}`,
-    changeFrequency: 'monthly',
-    priority: 0.7,
-  }));
+// export default function sitemap(): MetadataRoute.Sitemap {
+//   const pages: MetadataRoute.Sitemap = source.getPages().map((page) => ({
+//     url: `${baseUrl}${page.url}`,
+//     changeFrequency: 'monthly',
+//     priority: 0.7,
+//   }));
 
-  return [
-    {
-      url: baseUrl,
-      changeFrequency: 'weekly',
-      priority: 1,
-    },
-    ...pages,
-  ];
-}
+//   return [
+//     {
+//       url: baseUrl,
+//       changeFrequency: 'weekly',
+//       priority: 1,
+//     },
+//     ...pages,
+//   ];
+// }
